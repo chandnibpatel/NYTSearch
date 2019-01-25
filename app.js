771ac61cd86b5561647212e4202a8ce6511b11af
chandni <chandnibpatel@gmail.com>
2019-01-24 21:16:26 -0600

    Html created

---
+++ app.js
@@ -0,0 +1,9 @@
+//variables
+var searchItem = '';
+var numOfItems = 0;
+var year_Begin;
+var year_End;
+var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yYEVodyc3kMX6CfJLUDTHAAnTIUfgc9B'
+//functions
+
+//main process
